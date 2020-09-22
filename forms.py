6c463a0a467ce0de5c319a641351d375b3683de3
ScottBurden <scottalanburden@gmail.com>
2020-09-22 11:30:05 -0500

    added GradeAssignmentForm

--- forms.py
+++ forms.py
@@ -1,8 +1,12 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
-from wtforms.validators import InputRequired
+from wtforms import StringField, PasswordField, IntegerField
+from wtforms.validators import InputRequired, NumberRange
 
 class LoginForm(FlaskForm):
 
     username = StringField("Username", validators=[InputRequired()])
     password = PasswordField("Password", validators=[InputRequired()])
+
+class GradeAssignmentForm(FlaskForm):
+
+    grade = IntegerField("Grade", validators=[NumberRange(0,100)])
